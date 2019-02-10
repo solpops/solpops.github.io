1d71b1788e667e97e6599a70b8a4e1451685b23e
mspennyworth <sarah13mail@gmail.com>
2019-02-09 20:48:05 -0500

    initial publish for site

---
+++ old_index.php
@@ -0,0 +1,40 @@
+<html>
+<head>
+<title>Welcome to <?php echo $_SERVER["HTTP_HOST"]; ?></title>
+<meta http-equiv="Content-Type" content="text/html; charset=iso-8859-1">
+<link rel="stylesheet" href="http://www.canvasdreams.com/style.css">
+</head>
+<body bgcolor="ffffff">
+<p>&nbsp;</p>
+<p>&nbsp;</p>
+<table width="650" border="0" align="center" cellpadding="1" cellspacing="0" class="text" bgcolor="#BFD3DB">
+<tr>
+<td><table width="648" border="0" align="center" cellpadding="10" cellspacing="0" class="text">
+<tr>
+<td bgcolor="#FFFFFF">
+	
+<p align="center"><a target="_blank" href="http://www.canvasdreams.com"><img src="http://www.canvasdreams.com/art/logo.jpg" width="200" height="52" border="0"></a></p>
+
+<p class="head">Welcome to the future home of <?php echo $_SERVER["HTTP_HOST"]; ?>!</p>
+
+<p>This site is now being hosted by <a href="http://www.canvasdreams.com" target="_blank">Canvas Dreams LLC</a>. We offer complete domain name registration, web hosting, dedicated servers, and colocation services from our downtown Portland data facility.</p>
+
+<br>
+
+<p class="subhead">If you are the owner of <?php echo $_SERVER["HTTP_HOST"]; ?>:</p>
+
+<p>To replace this with your own home page, upload a new index.php file to your site root (located at /public_html/) using the FTP login instructions that were emailed to you when you signed up for service with us. You can also delete this file and upload an index.html file instead.</p>
+
+<p>You can also log into <a href="https://www.canvasdreams.net/account/">Customer Support</a> and request additional information or call us 24 hours a day at 800.574.4299.
+
+<p>Thank you for choosing Canvas Dreams. We look forward to providing you the very best Portland web hosting services possible.</p>
+
+<p align="center" class="subtext"><br>&nbsp;Copyright <a href="http://www.canvasdreams.com">Canvas Dreams LLC</a></p>
+ 
+</td>
+</tr>
+</table></td>
+</tr>
+</table>
+</body>
+</html>
